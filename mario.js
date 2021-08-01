e52dd196f2a7597093eca549aec31246d524a36f
Jordan <mehravarj+github@gmail.com>
2021-08-01 00:03:03 -0400

    Cleaned up code for readability

--- mario.js
+++ mario.js
@@ -1,34 +1,32 @@
 class Mario {
     constructor() {
+        this.audio = new Audio('sounds/jump.wav');
+
         this.image = new Image();
         this.image.src = "images/mario.png"
         this.imageWidth = 116;
         this.imageHeight = 124;
         this.imageSprite = 0;
-        this.audio = new Audio('sounds/jump.wav');
 
         this.width = 100;
         this.height = 120;
-        this.x = CANVAS_W * 0.2;
-        this.y = CENTER_Y - this.h;
+        this.xPos = 200;
+        this.yPos = 200;
 
         this.speed = 10;
         this.gravity = 7;
         this.wind = 3;
-        this.jump = 11;
 
-        this.moveLeft = this.speed - this.wind;
-        this.moveRight = this.speed + this.wind;
-        this.moveDown = this.speed + this.gravity;
-        this.moveUp;
-
+        
         this.isJumping = true;
-        this.isDead = false;
+        this.jumpHeight = 11;
+
     }
 
     move() {
-        if (leftHeld) this.x -= this.moveLeft;
-        if (rightHeld) this.x += this.moveRight;
-        if (downHeld) this.y += this.moveDown;
+        if (leftHeld) this.xPos -= this.speed - this.wind;
+        if (rightHeld) this.xPos += this.speed + this.wind;
+        if (downHeld) this.yPos += this.speed + this.gravity;
+        this.isJumping = spacePressed ? true : false;
     }
 }
