280c6c5d7945413c004bacaddd0de9e6c16da725
Jordan <mehravarj+github@gmail.com>
2021-08-04 00:01:59 -0400

    Added custom hitbox for all enemies

--- enemies.js
+++ enemies.js
@@ -1,13 +1,13 @@
 class Enemy {
     constructor() {
         this.imageSprite = 0;
-        this.xPos = CANVAS_W;
+        this.xPos = CENTER_X;
+        this.yPos = CENTER_Y;
         this.yIni = 0;
-        this.yPos = 0;
         this.time = 0;
         this.isWavey = false;
         this.canRespawn = false;
-
+        this.reflect = this.yIni < CENTER_Y ? 1 : -1
     }
 
     move() {
@@ -29,6 +29,10 @@ class SuperKoopaYellow extends Enemy {
         this.image.src = "images/super-koopa-yellow.png";
         this.respawnLine = CANVAS_W - 150;
         this.xSpeed = 15;
+        this.wBox = 72;
+        this.hBox = 45;
+        this.xOff = 5;
+        this.yOff = 5;
     }
 }
 
@@ -39,7 +43,11 @@ class SuperKoopaRed extends Enemy {
         this.image = new Image(92, 52);
         this.image.src = "images/super-koopa-red.png";
         this.respawnLine = CANVAS_W - 150;
-        this.speed = 17.5;
+        this.xSpeed = 17.5;
+        this.wBox = 72;
+        this.hBox = 45;
+        this.xOff = 5;
+        this.yOff = 5;
     }
 }
 
@@ -57,6 +65,11 @@ class ParakoopaRed extends Enemy {
         this.wave = 'cos'
         this.amplitude = 50;
         this.radians = this.time / 100;
+
+        this.wBox = 56;
+        this.hBox = 93;
+        this.xOff = 5;
+        this.yOff = 15;
     }
 }
 
@@ -74,6 +87,11 @@ class ParakoopaYellow extends Enemy {
         this.wave = 'sin'
         this.amplitude = 50;
         this.radians = this.time / 100;
+
+        this.wBox = 56;
+        this.hBox = 93;
+        this.xOff = 5;
+        this.yOff = 15;
     }
 }
 
@@ -91,6 +109,11 @@ class FlyingGoomba extends Enemy {
         this.wave = 'sin'
         this.amplitude = 50;
         this.radians = this.time / 100;
+
+        this.wBox = 70;
+        this.hBox = 60;
+        this.xOff = 25;
+        this.yOff = 35;
     }
 }
 
@@ -108,6 +131,11 @@ class ParakoopaBlue extends Enemy {
         this.wave = 'sin'
         this.amplitude = 150;
         this.radians = this.time / 200;
+
+        this.wBox = 56;
+        this.hBox = 93;
+        this.xOff = 5;
+        this.yOff = 15;
     }
 }
 
@@ -124,10 +152,14 @@ class ParakoopaGreen extends Enemy {
         this.wave = 'cos'
         this.amplitude = 150;
         this.radians = this.time / 200;
+
+        this.wBox = 56;
+        this.hBox = 93;
+        this.xOff = 5;
+        this.yOff = 15;
     }
 }
 
-//// I"M HERE
 class FlyingBrother extends Enemy {
     constructor() {
         super();
@@ -138,27 +170,35 @@ class FlyingBrother extends Enemy {
         this.spriteRate = 200;
         this.respawnLine = CENTER_X;
         this.xSpeed = 15;
+
+        this.wBox = 120;
+        this.hBox = 136;
+        this.xOff = 49;
+        this.yOff = 14;
     }
 
     move() {
-        animate(this);
-        let direction = this.y0 < CENTER_Y ? 1 : -1
-        this.x -= this.speed;
-        this.y = this.y0 + 4*Math.sqrt(Math.abs(8*this.time))*direction;
+        let reflect = this.y0 < CENTER_Y ? 1 : -1
+        this.xPos -= this.xSpeed;
+        this.yPos = this.yIni + 4 * reflect * 
+        Math.sqrt(Math.abs(8*this.time));        
     }
 }
 
 class Chainsaw extends Enemy {
     constructor() {
         super();
-        
+
+        this.image = new Image(64, 176);
         this.image.src = "images/chainsaw.png";
         this.spriteTotal = 4;
         this.spriteRate = 50;
         this.respawnLine = CENTER_X - 200;
-        this.w = 64;
-        this.h = 176;
-        this.speed = BG_SPEED;
+        this.xSpeed = 2//BG_SPEED;
+        this.wBox = 50;
+        this.hBox = 168;
+        this.xOff = 7;
+        this.yOff = 3;
     }
 }
 
@@ -166,16 +206,19 @@ class BigBoo extends Enemy {
     constructor() {
         super();
         
+        this.image = new Image(268, 256);
         this.image.src = "images/big-boo.png";
         this.respawnLine = CENTER_X - 200;
-        this.w = 268;
-        this.h = 256;
-        this.speed = 14;
-        this.customHitbox = true;
-        this.xHitbox = this.x + 20
-        this.yHitbox = this.y + 15
-        this.wHitbox = this.w - 40
-        this.hHitbox = this.h - 30
+        this.xSpeed = 14;
+        this.isWavey = true;
+        this.wave = 'sin'
+        this.amplitude = 100;
+        this.radians = this.time / 1000;    
+
+        this.wBox = 202;
+        this.hBox = 218;
+        this.xOff = 30;
+        this.yOff = 20;
     }
 }
 
@@ -183,28 +226,23 @@ class BigBubble extends Enemy {
     constructor() {
         super();
         
+        this.image = new Image(224, 240);
         this.image.src = "images/big-bubble.png";
         this.respawnLine = CENTER_X - 200;
         this.spriteTotal = 2;
         this.spriteRate = 250;
-        this.w = 224;
-        this.h = 240;
         this.xSpeed = 8;
         this.ySpeed = 8;
-        this.customHitbox = true;
-        this.xHitbox = this.x + 20
-        this.yHitbox = this.y + 20
-        this.wHitbox = this.w - 40
-        this.hHitbox = this.h - 40
+        this.wBox = 184;
+        this.hBox = 200;
+        this.xOff = 20;
+        this.yOff = 20;
     }
 
     move() {
-        animate(this);
         if (this.y + this.h > CANVAS_H || this.y < 0) this.ySpeed = -this.ySpeed;
-        this.x -= this.xSpeed;
-        this.y += this.ySpeed;
-        this.xHitbox = this.x + 20;
-        this.yHitbox = this.y + 20;
+        this.xPos -= this.xSpeed;
+        this.yPos += this.ySpeed;
     }
 }
 
@@ -213,13 +251,16 @@ class BooBuddy extends Enemy {
         super();
         
         this.boo = ["red","black","tongue"][Math.floor(Math.random()*3)],
+        this.image = new Image(64, 64);
         this.image.src = "images/boo-buddy-" + this.boo + ".png";
         this.spriteTotal = 2
         this.spriteRate = 150;
         this.respawnLine = CANVAS_W - 150;
-        this.w = 64;
-        this.h = 64;
-        this.speed = 20;
+        this.xSpeed = 20;
+        this.wBox = 64;
+        this.hBox = 64;
+        this.xOff = 0;
+        this.yOff = 0;
     }
 }
 
@@ -227,14 +268,21 @@ class Eerie extends Enemy {
     constructor() {
         super();
         
+        this.image = new Image(64, 64)
         this.image.src = "images/eerie.png";
         this.spriteTotal = 2;
         this.spriteRate = 100;
         this.respawnLine = CANVAS_W - 150;
-        this.w = 64;
-        this.h = 64;
-        this.speed = 12;
-        this.coin = Math.round(Math.random())
+        this.xSpeed = 12;
+        this.coin = Math.round(Math.random());
+        this.isWavey = true;
+        this.wave = this.coin ? "sin" : "cos;"
+        this.amplitude = 40 * (this.time / 800);
+        this.radians = 15 * this.time / 800;
+        this.wBox = 64;
+        this.hBox = 64;
+        this.xOff = 0;
+        this.yOff = 0;;
     }
 }
 
@@ -242,36 +290,37 @@ class BanzaiBill extends Enemy {
     constructor() {
         super();
         
+        this.image = new Image(256, 256);
         this.image.src = "images/banzai-bill.png";
         this.respawnLine = CENTER_X + 256;
-        this.w = 256;
-        this.h = 256;
-        this.speed = 18;
-        this.customHitbox = true;
-        this.xHitbox = this.x + 20;
-        this.yHitbox = this.y + 25;
-        this.wHitbox = this.w - 20;
-        this.hHitbox = this.h - 30;
+        this.xSpeed = 18;
+        this.wBox = 246;
+        this.hBox = 236;
+        this.xOff = 10;
+        this.yOff = 10;
     }
 }
 
 class BulletBillLinear extends Enemy {
     constructor() {
         super();
-        
+ 
+        this.image = new Image(64, 56);
         this.image = new Image();
         this.image.src = "images/bullet-bill-linear.png";
         this.respawnLine = CANVAS_W;
-        this.w = 64;
-        this.h = 56;
-        this.speed = 35;
-        
+        this.xSpeed = 35;
+        this.wBox = 64;
+        this.hBox = 56;
+        this.xOff = 0;
+        this.yOff = 0;
     }
 }
 
 class BulletBillDiagonal extends Enemy {
     constructor() {
         super();
+        this.image = new Image(64, 64);
         this.upSrc = "images/bullet-bill-diagonal-up.png";
         this.downSrc = "images/bullet-bill-diagonal-down.png"
         this.corner = Math.round(Math.random()) * CANVAS_H;
@@ -288,10 +337,12 @@ class BulletBillDiagonal extends Enemy {
 
         
         this.respawnLine = CANVAS_W;
-        this.w = 64;
-        this.h = 64;
-        this.x = CANVAS_W - this.w;
-        this.speed = 22;
+        this.xPos = CANVAS_W - this.w;
+        this.xSpeed = 22;
+        this.wBox = 64;
+        this.hBox = 64;
+        this.xOff = 0;
+        this.yOff = 0;
     }
 
     move() {
@@ -303,19 +354,17 @@ class BulletBillDiagonal extends Enemy {
 class Grinder extends Enemy {
     constructor() {
         super();
-        
+ 
+        this.image = new Image (128, 128);
         this.image.src = "images/grinder.png";
         this.spriteTotal = 2;
         this.spriteRate = 25;
         this.respawnLine = CENTER_X + 100;
-        this.w = 128;
-        this.h = 128;
-        this.speed = BG_SPEED;
-        this.customHitbox = true;
-        this.xHitbox = this.x + 12;
-        this.yHitbox = this.y + 12;
-        this.wHitbox = this.w - 24;
-        this.hHitbox = this.h - 24;
+        this.xSpeed = 2 //BG_SPEED;
+        this.wBox = 108;
+        this.hBox = 108;
+        this.xOff = 10;
+        this.yOff = 10;
     }
 }
 
