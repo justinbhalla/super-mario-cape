9e1552df79f0e5af5190138fc9d2f713320c4b81
Jordan <mehravarj+github@gmail.com>
2021-08-03 20:33:12 -0400

    Created a helper function for moving hitboxes

--- helpers.js
+++ helpers.js
@@ -1,3 +1,10 @@
 // hitbox code
 // ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+function moveHitbox(element) {
+    let {xPos, yPos, xOff, yOff} = element;
+    element.xBox = xPos + xOff;
+    element.yBox = yPos + yOff;
+}  
+
+// Hit box helper
 // ctx.fillRect(element.xBox, element.yBox, wBox, hBox);
