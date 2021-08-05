e16916a74ca83281988da5b234f14f74db11ee5a
Jordan <mehravarj+github@gmail.com>
2021-08-05 00:50:37 -0400

    Added detectHit function for enemies

--- helpers.js
+++ helpers.js
@@ -3,3 +3,24 @@ function moveHitbox(element) {
     element.xBox = xPos + xOff;
     element.yBox = yPos + yOff;
 }
+
+function detectHit(enemy) {
+    let {
+        xBox: ex1,
+        yBox: ey1,
+        wBox: ew,
+        hBox: eh
+    } = enemy;
+
+    let {
+        xBox: mx1,
+        yBox: my1,
+        wBox: mw,
+        hBox: mh
+    } = player;
+
+    let [ex2, mx2] = [ex1 + ew, mx1 + mw];
+    let [ey2, my2] = [ey1 + eh, my1 + mh];
+
+    return !(ex1 >= mx2 || ey1 >= my2 || ex2 <= mx1 || ey2 <= my1);
+}
