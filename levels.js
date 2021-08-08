44b89f293698cc1201d99ae2e897bf2c037602ac
JMMFL <mehravarj+github@gmail.com>
2021-08-08 17:03:50 -0400

    Moved levels to constants.js

--- levels.js
+++ levels.js
@@ -4,14 +4,3 @@ class Level {
         this.volume = volume;
     }
 }
-
-const LEVEL_1 = new Level("overworld", 0.4);
-const LEVEL_2 = new Level("athletic", 0.45);
-const LEVEL_3 = new Level("haunted", 0.35);
-const LEVEL_4 = new Level("castle", 0.8);
-const FINALE = new Level("ending", 0.5);
-
-LEVEL_1.spawner = () => {}
-LEVEL_2.spawner = () => {}
-LEVEL_3.spawner = () => {}
-LEVEL_4.spawner = () => {}
