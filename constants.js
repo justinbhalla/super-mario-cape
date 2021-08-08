5781006acea03479807d5258084189655ebad156
JMMFL <mehravarj+github@gmail.com>
2021-08-08 18:38:14 -0400

    Added control flags from controls.js

--- constants.js
+++ constants.js
@@ -11,11 +11,20 @@ background.style.backgroundPositionX = 0;
 background.style.backgroundPositionY = 0;
 const BG_SPEED = 5;
 
-
 let gameState = "START";
 let gamePlayer = new Mario();
 let gameElements = [];
 let gameLevel = -1;
+
+const LEFT = 37;
+const RIGHT = 39;
+const DOWN = 40;
+const UP = 38;
+const ENTER = 13;
+let leftHeld = false;
+let rightHeld = false;
+let downHeld = false;
+let upHeld = false;
 
 const LEVEL_1 = new Level("overworld", 0.4);
 const LEVEL_2 = new Level("athletic", 0.45);
@@ -23,8 +32,3 @@ const LEVEL_3 = new Level("haunted", 0.35);
 const LEVEL_4 = new Level("castle", 0.8);
 const FINALE = new Level("ending", 0.5);
 const LEVELS = [LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4];
-
-LEVEL_1.spawn = () => {}
-LEVEL_2.spawn = () => {}
-LEVEL_3.spawn = () => {}
-LEVEL_4.spawn = () => {}
