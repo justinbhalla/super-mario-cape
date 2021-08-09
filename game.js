d32eb4e2b46ceb143476cfffc9a7b0d719eb4b98
JMMFL <mehravarj+github@gmail.com>
2021-08-09 19:57:52 -0400

    Split render loop into two loops and added menu / scenes

--- game.js
+++ game.js
@@ -1,20 +1,65 @@
-function run() {   
+function update() {   
     ctx.clearRect(0,0, CANVAS_W, CANVAS_H);
-    
-    if (gameState !== "DEAD") {
-        for (let element of gameElements) {
-            element.move();
-            element.update();
-        }
-    
-        gamePlayer.move();
-        scrollBackground();
-    }
-
-    drawImage(gamePlayer);
-    requestAnimationFrame(run);
+    gameElements.update();
+    gamePlayer.update();
+    updateReq = requestAnimationFrame(update);
 }
 
-run();
-document.addEventListener("keydown", gameControls);
-document.addEventListener("keyup", gameControls);
+function move() {
+    gameElements.move();
+    gamePlayer.move();
+    scrollBackground();
+    moveReq = requestAnimationFrame(move);
+}
+
+function menu(e) {
+    if (e.keyCode === UP) {
+        switch(gameState) {
+            case "START":
+                changeScreen(titleScreen, false);
+                gameState = "PLAY";
+                gameLevel++
+                LEVELS[gameLevel].spawn();
+                break;
+            case "DEAD":
+                changeScreen(deathScreen, false);
+                gamePlayer.spriteFrame = 0;
+                gamePlayer.xPos = 200;
+                gamePlayer.yPos = 200;
+                gameElements.length = 0;
+                gameState = "PLAY";
+                LEVELS[gameLevel].spawn();
+                break;
+        }
+    }
+}
+
+function deathScene() {
+    setTimeout(() => cancelAnimationFrame(moveReq));
+    gamePlayer.spriteFrame = 3;
+    let yPeak = gamePlayer.yPos - 200;
+    let yFlag = true;
+
+    setTimeout(() => {
+        let animate = setInterval(() => {
+            let yPos = gamePlayer.yPos;
+
+            if (yPos > CANVAS_H) {
+                clearInterval(animate);
+                gameState = "DEAD";
+                deathScreen.style.display = "block";
+            } else if (yPos > yPeak && yFlag) {
+                gamePlayer.yPos -= 10;
+            } else if (yPos < yPeak && yFlag) {
+                yFlag = false;
+            } else {
+                gamePlayer.yPos += 12.5;
+            }
+        }, 10);
+    }, 500)
+}
+
+
+move();
+update();
+document.addEventListener("keydown", menu);
