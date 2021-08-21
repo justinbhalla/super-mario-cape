2a09e98228a171df07d10554ddd7ef9ccd8b65d4
JMMFL <mehravarj+github@gmail.com>
2021-08-21 10:13:34 -0400

    Added audio files to loading process. Changed format.

--- modules/load.js
+++ modules/load.js
@@ -1,22 +1,42 @@
 let loading = [];
-let images = Array.from([
-    "atlas.png",
-    "sound-on.png",
-    "sound-off.png",
-    "lives.png"
-], load);
 
-function load(asset) {
-    let ext = asset.slice(asset.length - 3);
-    let isImage = ext === "png" || ext === "jpg";
-    let element = isImage ? new Image() : new Audio();
-    element.src = `${isImage ? "images" : "sounds"}/${asset}`;
-    element[isImage ? "onload" : "oncanplaythrough"] = function() {
-        let index = loading.indexOf(this);
-        if (index > -1) loading.splice(index, 1);
+function load(assets) {
+    for (let a of assets) {
+        let ext = a.slice(a.length - 3);
+        let isImage = ext === "png";
+        let element = isImage ? new Image() : new Audio();
+        element.src = `${isImage ? "images" : "sounds"}/${a}`;
+        element[isImage ? "onload" : "oncanplaythrough"] = function() {
+            let index = loading.indexOf(this);
+            if (index > -1) loading.splice(index, 1);
+        }
+        
+        loading.push(element);
     }
-
-    loading.push(element);
 }
 
-export { loading, images }
+load([
+
+// IMAGES
+"atlas.png",
+"sound-on.png",
+"sound-off.png",
+"lives.png",
+
+// SOUND
+"died.wav",
+"course.wav",
+"fortress.wav",
+"iris.wav",
+"over.wav",
+"jump.wav",
+"athletic.mp3",
+"castle.mp3",
+"title.mp3",
+"ending.mp3",
+"spooky.mp3",
+"overworld.mp3"
+
+]);
+
+export { loading }
