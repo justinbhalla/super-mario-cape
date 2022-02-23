59cfd1a88dd92f8bf8f75fb98c2c37f37c642b34
JMMFL <mehravarj+github@gmail.com>
2022-02-22 20:18:42 -0500

    Merged controller object and event listeners for player/menu input.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { screens, storyboard } from './modules/scenes.js';
-import playerControls from './modules/controller.js';
+import controller from './modules/controller.js';
 import { Mario } from './modules/elements.js';
 import { loading } from './modules/load.js';
 
@@ -39,25 +39,6 @@ const game = {
     fortressClear: new Audio('audio/sfx/fortress-clear.wav'),
     messageBlock: new Audio('audio/sfx/message-block.wav'),
     gameOver: new Audio('audio/sfx/game-over.wav'),
-  },
-};
-
-const controller = {
-  LEFT_KEY: 37,
-  RIGHT_KEY: 39,
-  UP_1_KEY: 38,
-  UP_2_KEY: 32,
-  DOWN_KEY: 40,
-  pressedLeft: false,
-  pressedRight: false,
-  pressedUp: false,
-  pressedDown: false,
-
-  reset() {
-    this.pressedLeft = false;
-    this.pressedRight = false;
-    this.pressedUp = false;
-    this.pressedDown = false;
   },
 };
 
@@ -108,36 +89,6 @@ function drawBackground() {
   context.clearRect(0, 0, PIXELS.width, PIXELS.height);
 }
 
-function menuControls(event) {
-  if (game.isOn) return;
-
-  let { keyCode, type } = event;
-  let { UP_1_KEY, UP_2_KEY } = controller;
-  let menuClicked = type === 'click';
-  let menuPressed = keyCode === UP_1_KEY || keyCode === UP_2_KEY;
-
-  if (menuClicked || menuPressed) {
-    switch (game.state) {
-      case 'START':
-        storyboard.showSceneTutorial();
-        break;
-      case 'RETRY':
-        // hideScreen(screens.retry);
-        // game.lives--;
-        //levelScene();
-        break;
-      case 'OVER':
-        // hideScreen(screens.failure);
-        // game.lives = game.livesStart;
-        // game.level = 0;
-        // levelScene();
-        break;
-      default:
-        break;
-    }
-  }
-}
-
 function setGame() {
   let interval = setInterval(() => {
     if (loading.length === 0) {
@@ -162,14 +113,13 @@ function setAudio() {
 
 function setControls() {
   const startBtn = document.getElementById('screen-intro__start-btn');
-  startBtn.addEventListener('click', menuControls);
-  // document.addEventListener('keydown', menuControls);
-  document.addEventListener('keydown', playerControls);
-  document.addEventListener('keyup', playerControls);
+  startBtn.addEventListener('click', controller.onMenuInput);
+  document.addEventListener('keydown', controller.onPlayerInput);
+  document.addEventListener('keyup', controller.onPlayerInput);
 }
 
 window.addEventListener('load', setGame);
 
 export { LEVELS, Level } from './modules/levels.js';
 export * as Enemies from './modules/elements.js';
-export { PIXELS, context, game, controller, elements };
+export { PIXELS, context, game, controller, storyboard, elements };
--- modules/controller.js
+++ modules/controller.js
@@ -1,32 +1,84 @@
-import { game, controller } from '../main.js';
+import { game, storyboard } from '../main.js';
 
-export default function playerControls(e) {
-  if (!game.isOn) return;
+const controller = {
+  LEFT_KEY: 37,
+  RIGHT_KEY: 39,
+  UP_1_KEY: 38,
+  UP_2_KEY: 32,
+  DOWN_KEY: 40,
+  pressedLeft: false,
+  pressedRight: false,
+  pressedUp: false,
+  pressedDown: false,
 
-  let isHeld = e.type === 'keydown';
-  let jumpFlag = false;
+  reset() {
+    this.pressedLeft = false;
+    this.pressedRight = false;
+    this.pressedUp = false;
+    this.pressedDown = false;
+  },
 
-  switch (e.keyCode) {
-    case controller.LEFT_KEY:
-      controller.pressedLeft = isHeld;
-      break;
-    case controller.RIGHT_KEY:
-      controller.pressedRight = isHeld;
-      break;
-    case controller.DOWN_KEY:
-      controller.pressedDown = isHeld;
-      break;
-    case controller.UP_1_KEY:
-    case controller.UP_2_KEY:
-      if (!jumpFlag && isHeld) {
-        jumpFlag = true;
-        // sounds.jump.currentTime = 0;
-        // playSound(sounds.jump);
-      } else if (!isHeld) {
-        jumpFlag = false;
+  onMenuInput(event) {
+    if (game.isOn) return;
+
+    let { keyCode, type } = event;
+    let { UP_1_KEY, UP_2_KEY } = this;
+    let menuClicked = type === 'click';
+    let menuPressed = keyCode === UP_1_KEY || keyCode === UP_2_KEY;
+
+    if (menuClicked || menuPressed) {
+      switch (game.state) {
+        case 'START':
+          storyboard.showSceneTutorial();
+          break;
+        case 'RETRY':
+          // hideScreen(screens.retry);
+          // game.lives--;
+          //levelScene();
+          break;
+        case 'OVER':
+          // hideScreen(screens.failure);
+          // game.lives = game.livesStart;
+          // game.level = 0;
+          // levelScene();
+          break;
+        default:
+          break;
       }
+    }
+  },
 
-      controller.pressedUp = isHeld;
-      break;
-  }
-}
+  onPlayerInput(event) {
+    if (!game.isOn) return;
+
+    let { LEFT_KEY, RIGHT_KEY, DOWN_KEY, UP_1_KEY, UP_2_KEY } = controller;
+    let isHeld = event.type === 'keydown';
+    let jumpFlag = false;
+
+    switch (event.keyCode) {
+      case LEFT_KEY:
+        controller.pressedLeft = isHeld;
+        break;
+      case RIGHT_KEY:
+        controller.pressedRight = isHeld;
+        break;
+      case DOWN_KEY:
+        controller.pressedDown = isHeld;
+        break;
+      case UP_1_KEY:
+      case UP_2_KEY:
+        if (!jumpFlag && isHeld) {
+          jumpFlag = true;
+          // sounds.jump.currentTime = 0;
+          // playSound(sounds.jump);
+        } else if (!isHeld) {
+          jumpFlag = false;
+        }
+
+        controller.pressedUp = isHeld;
+        break;
+    }
+  },
+};
+
+export { controller as default };
